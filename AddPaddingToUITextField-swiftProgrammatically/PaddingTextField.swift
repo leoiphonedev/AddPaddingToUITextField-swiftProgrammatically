21718abc2aa01166e9677eab951ebf75522f103f
leoiphonedev <aggarwalaman1985@gmail.com>
2018-07-23 22:52:37 +0530

    Pushing code

---
+++ AddPaddingToUITextField-swiftProgrammatically/PaddingTextField.swift
@@ -0,0 +1,36 @@
+//
+//  PaddingTextField.swift
+//  AddPaddingToUITextField-swiftProgrammatically
+//
+//  Created by Aman Aggarwal on 23/07/18.
+//  Copyright © 2018 Aman Aggarwal. All rights reserved.
+//
+
+import UIKit
+
+class PaddingTextField: UITextField {
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, UIEdgeInsetsMake(0, 5, 0, 15))
+    }
+    
+    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, UIEdgeInsetsMake(0, 5, 0, 15))
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, UIEdgeInsetsMake(0, 5, 0, 15))
+    }
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
